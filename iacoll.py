6513ab151dde494f3b61a2272425dec47a01160f
Ed Summers <ehs@pobox.com>
2019-01-01 19:49:26 +0000

    write to leveldb instead of gzip

--- iacoll.py
+++ iacoll.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
 """
-usage: iacoll <ia-collection-id> <gzip-file-path>
+usage: iacoll <ia-collection-id>
 
 iacoll will collect all the item metadata for an Internet Archive collection
-and store it as line-oriented json in a gzipped file.
+and store it in a leveldb database. 
 
-If you run it more than once it will scan the gzipped file for the most recent
-item and only append newer records.
+If you run it more than once with the same database it will only fetch records
+since the last run.
 """
 
 import os
 import sys
 import gzip
 import json
+import plyvel
 import logging
 import argparse
 import internetarchive as ia
@@ -24,9 +25,9 @@ from dateutil.parser import parse
 def main():
     ap = argparse.ArgumentParser('collect Internet Archive collectian metadata')
     ap.add_argument('collection_id', help='Internet Archive Collection ID')
-    ap.add_argument('path', help='Path to write gzipped data to')
+    ap.add_argument('--db', help='Location of leveldb database')
+    ap.add_argument('--fullscan', action="store_true", help='Examine all search results, useful to fill in gaps if an update failed')
     ap.add_argument('--log', help='Path to write log', default='iacoll.log')
-    ap.add_argument('--resume', action="store_true", help='Resume failed update')
     args = ap.parse_args()
 
     logging.basicConfig(
@@ -35,66 +36,60 @@ def main():
         format="%(asctime)s %(levelname)s %(message)s"
     )
 
-    first_id, last_id, total_items_saved = get_archive_info(args.path)
-    logging.info('found last id %s', last_id)
-    logging.info('found first id %s', first_id)
-    logging.info('found %s saved records', total_items_saved)
-    total_items = get_item_count(args.collection_id)
+    if args.db:
+        db_path = args.db
+    else:
+        db_path = '%s.db' % args.coll_id
 
-    if args.resume:
+    db = plyvel.DB(db_path, create_if_missing=True)
+    total_items = get_item_count(args.collection_id)
+    total_items_saved = get_total_items_saved(db)
+
+    if args.fullscan:
         progress = tqdm(total=total_items, unit='records')
         progress.update(total_items_saved)
     else:
         progress = tqdm(total=total_items - total_items_saved, unit='records')
 
-    output = gzip.open(args.path, 'at')
-    for item in get_items(args.collection_id, first_id, last_id, args.resume):
-        output.write(json.dumps(item) + '\n')
+    for item in get_items(args.collection_id, db, args.fullscan)
         progress.update(1)
-        logging.info("saved %s" % item['metadata']['identifier'])
+
+    db.close()
 
 def get_item_count(coll_id):
     return len(ia.search_items('collection:%s' % coll_id))
 
-def get_items(coll_id, first_id=None, last_id=None, resume=False):
-    seen_first = False
-    seen_last = False
-    for result in ia.search_items('collection:%s' % coll_id, sorts=['addeddate desc']):
-        if result['identifier'] == last_id:
-            seen_last = True
-        if result['identifier'] == first_id:
-            seen_first = True
+def get_total_items_saved(db):
+    count + 0
+    for k, v in db.iterator(start=b'iacoll:item:', end=b'iacoll:item;'):
+        count += 1
+    return count
 
-        if not resume and seen_last:
+def get_items(coll_id, db, last_id=None, fullscan=False):
+    new_last_id = None
+    for result in ia.search_items('collection:%s' % coll_id, sorts=['addeddate desc']):
+        if new_last_id == None:
+            new_last_id = result['identifier']
+
+        if result['identifier'] == last_id and not fullscan:
+            logging.info('found last id %s, stopping', last_id)
             break
 
-        if resume and seen_last and not seen_first:
+        item_key = get_item_key(result['identifier'])
+        if db.get(item_key):
+            logging.info('already saw %s, skippping', result['identifier'])
             continue
 
         item = ia.get_item(result['identifier'])
+        db.put(item_key, bytes(json.dump(item.item_metadata), 'utf8'))
+        logging.info("saved %s" % item['metadata']['identifier'])
+
         yield(item.item_metadata)
 
-def get_archive_info(path):
-    if not os.path.isfile(path):
-        return None, 0
-    count = 0
-    last_id = None
-    first_id = None
-    max_date = None
-    min_date = None
-    for line in gzip.open(open(path, 'rb')):
-        count += 1
-        item = json.loads(line)
-        if 'metadata' not in item or 'addeddate' not in item['metadata']:
-            continue
-        date = parse(item['metadata']['addeddate'])
-        if max_date is None or date > max_date:
-            max_date = date
-            last_id = item['metadata']['identifier']
-        if min_date is None or date < min_date:
-            min_date = date
-            first_id = item['metadata']['identifier']
-    return first_id, last_id, count
+    db.put('iacoll:last-item-id' % b(new_last_item_id, 'utf8'))
+
+def get_item_key(id):
+    return bytes('iacoll:item:%s' % id, 'utf8')
 
 if __name__  == "__main__":
     main()
