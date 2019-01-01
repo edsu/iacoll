4cd28118b22ae2161d06c08d1a30790ffb37aa44
Ed Summers <ehs@pobox.com>
2019-01-01 20:56:14 +0000

    added dump command

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
 # iacoll
 
 *iacoll* will collect all the item metadata for an Internet Archive collection
-and store it as line-oriented json in a gzipped file.
+and store it in a LevelDB database. The database is a key value store where the
+key is the unique Internet Archive item identifier, and the value is the JSON
+for the item metadata.
 
 For example you can download the metadata for items in the University of
 Maryland's collection:
 
-    % iacoll university_maryland_cp university_maryland_cp.jsonl.gz
+    % iacoll university_maryland_cp 
 
-If you run it more than once it will scan the gzipped file for the most recent
-item and only append newer records.
+By default `iacoll` will create the LevelDB database in a directory named with
+the item identifier. If you would like to control this you can explicitly pass
+it:
+
+    % iacoll university_maryland_cp --db /path/to/my/leveldb/database
+
+When you run `iacoll` repeatedly it will look at the database and only fetch
+newer records. If an update ever fails you may want to force a full rescan:
+
+    % iacoll university_maryland_cp --fullrescan
+
+If you would like to dump the metadata as line oriented JSON you can use --dump:
+
+    % iacoll university_maryland_cp --dump > university_maryland_cp.jsonl
 
 ## Install
 
--- iacoll.py
+++ iacoll.py
@@ -28,6 +28,7 @@ def main():
     ap.add_argument('--db', help='Location of leveldb database')
     ap.add_argument('--fullscan', action="store_true", help='Examine all search results, useful to fill in gaps if an update failed')
     ap.add_argument('--log', help='Path to write log', default='iacoll.log')
+    ap.add_argument('--dump', action='store_true', help='Dump metadata in db to stdout')
     args = ap.parse_args()
 
     logging.basicConfig(
@@ -39,11 +40,19 @@ def main():
     if args.db:
         db_path = args.db
     else:
-        db_path = '%s.db' % args.coll_id
+        db_path = args.collection_id
 
     db = plyvel.DB(db_path, create_if_missing=True)
     total_items = get_item_count(args.collection_id)
     total_items_saved = get_total_items_saved(db)
+
+    if args.dump:
+        dump(db)
+        sys.exit()
+
+    if total_items - total_items_saved == 0:
+        logging.info('no new items in %s', args.collection_id)
+        sys.exit()
 
     if args.fullscan:
         progress = tqdm(total=total_items, unit='records')
@@ -51,21 +60,27 @@ def main():
     else:
         progress = tqdm(total=total_items - total_items_saved, unit='records')
 
-    for item in get_items(args.collection_id, db, args.fullscan)
-        progress.update(1)
+    try:
+        for item in get_items(args.collection_id, db, args.fullscan):
+            progress.update(1)
+    except KeyboardInterrupt:
+        pass
 
+    progress.close()
     db.close()
 
 def get_item_count(coll_id):
     return len(ia.search_items('collection:%s' % coll_id))
 
 def get_total_items_saved(db):
-    count + 0
-    for k, v in db.iterator(start=b'iacoll:item:', end=b'iacoll:item;'):
+    count = 0
+    for k, v in db.iterator(start=b'iacoll:item:', stop=b'iacoll:item;'):
         count += 1
     return count
 
-def get_items(coll_id, db, last_id=None, fullscan=False):
+def get_items(coll_id, db, fullscan=False):
+    last_id = db.get(b'iacoll:last-item-id', b'').decode('utf8')
+    logging.info('found last item id %s', last_id)
     new_last_id = None
     for result in ia.search_items('collection:%s' % coll_id, sorts=['addeddate desc']):
         if new_last_id == None:
@@ -81,15 +96,18 @@ def get_items(coll_id, db, last_id=None, fullscan=False):
             continue
 
         item = ia.get_item(result['identifier'])
-        db.put(item_key, bytes(json.dump(item.item_metadata), 'utf8'))
-        logging.info("saved %s" % item['metadata']['identifier'])
+        db.put(item_key, bytes(json.dumps(item.item_metadata), 'utf8'))
+        logging.info("saved %s", result['identifier'])
 
+        db.put(b'iacoll:last-item-id', bytes(new_last_id, 'utf8'))
         yield(item.item_metadata)
-
-    db.put('iacoll:last-item-id' % b(new_last_item_id, 'utf8'))
 
 def get_item_key(id):
     return bytes('iacoll:item:%s' % id, 'utf8')
+
+def dump(db):
+    for k, v in db.iterator(start=b'iacoll:item:', stop=b'iacoll:item;'):
+        sys.stdout.write(v.decode('utf8') + '\n')
 
 if __name__  == "__main__":
     main()
