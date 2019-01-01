6fd0bd305e6db4fa927ef0dd76b8fe9517e4f2ff
Ed Summers <ehs@pobox.com>
2019-01-01 22:10:31 +0000

    upload markdown

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name='iacoll',
     version='0.0.1',
@@ -8,6 +11,8 @@ setup(
     author_email='ehs@pobox.com',
     py_modules=['iacoll', ],
     description='Collect metadata for Internet Archive collections',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     python_requires='>=3.6',
     install_requires=['internetarchive', 'tqdm', 'plyvel'],
     entry_points={'console_scripts': ['iacoll' = 'iacoll:main']}
